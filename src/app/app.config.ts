1f74483e3a1482aa00d8bc062d32b1f963ef462e
Ahmed Khaled <akhaledafify@gmail.com>
2026-05-03 18:37:41 +0300

    fix: rating stars not visible due to missing PrimeNG provider (added Aura theme + styling improvements)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,19 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { tmdbInterceptor } from './core/interceptor/tmdb-interceptor';
 import { loadingInterceptor } from './core/interceptor/loading-interceptor';
 import { errorInterceptor } from './core/interceptor/error-interceptor';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     provideHttpClient(withInterceptors([loadingInterceptor, tmdbInterceptor, errorInterceptor])),
   ],
 };
